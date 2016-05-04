f558fc1a035f472943fa44a70f8dd9ac5ef46afb
Seil <kaya.helloworld03@gmail.com>
2016-05-05 00:01:54 +0900

    .

--- app/src/main/java/com/kokushiseiya/tree/LoginActivity.java
+++ app/src/main/java/com/kokushiseiya/tree/LoginActivity.java
@@ -9,12 +9,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-/**
- * Created by YK on 16/05/04.
- */
+
 public class LoginActivity extends Activity {
     EditText userName;
     EditText passWord;
+    String nameKey;
+    String passKey;
     SharedPreferences key;
 
     @Override
@@ -22,25 +22,27 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        userName = (EditText)findViewById(R.id.userName_edit);
-        passWord = (EditText)findViewById(R.id.passWord_edit);
+        userName = (EditText) findViewById(R.id.userName_edit);
+        passWord = (EditText) findViewById(R.id.passWord_edit);
     }
 
-    public void signUp(View v){
-        Intent intent = new Intent(LoginActivity.this,SignupActivity.class);
+    public void signUp(View v) {
+        Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
         startActivity(intent);
     }
 
-    public void logIn(View v){
+    public void logIn(View v) {
         String userName_login = userName.getText().toString();
         String passWord_login = passWord.getText().toString();
 
-        key = getSharedPreferences(userName_login, Context.MODE_PRIVATE);
-        String strKey = key.getString(passWord_login, "null");
-        if (strKey == "null"){
+        key = getSharedPreferences("key", Context.MODE_PRIVATE);
+        nameKey = key.getString("userName_key", "null");
+        passKey = key.getString("passWord_key", "null");
+
+        if (!nameKey.equals(userName_login) || !passKey.equals(passWord_login)) {
             Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
-        }else {
-            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+        } else if (nameKey.equals(userName_login) && passKey.equals(passWord_login)) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
         }
     }
--- app/src/main/java/com/kokushiseiya/tree/SignupActivity.java
+++ app/src/main/java/com/kokushiseiya/tree/SignupActivity.java
@@ -8,10 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-/**
- * Created by YK on 16/05/04.
- */
-public class SignupActivity extends Activity{
+
+public class SignupActivity extends Activity {
     EditText userName;
     EditText passWord;
     SharedPreferences key;
@@ -20,20 +18,20 @@ public class SignupActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        userName = (EditText)findViewById(R.id.userName_edit);
-        passWord = (EditText)findViewById(R.id.passWord_edit);
-        key = getSharedPreferences("UserName", Context.MODE_PRIVATE);
+        userName = (EditText) findViewById(R.id.userName_edit);
+        passWord = (EditText) findViewById(R.id.passWord_edit);
+        key = getSharedPreferences("key", Context.MODE_PRIVATE);
     }
 
-    public void signUp(View v){
+    public void signUp(View v) {
         String userName_input = userName.getText().toString();
         String passWord_input = passWord.getText().toString();
-
         SharedPreferences.Editor editor = key.edit();
-        editor.putString(passWord_input, userName_input);
+        editor.putString("userName_key", userName_input);
+        editor.putString("passWord_key", passWord_input);
         editor.commit();
 
-        Intent intent = new Intent(SignupActivity.this,MainActivity.class);
+        Intent intent = new Intent(SignupActivity.this, MainActivity.class);
         startActivity(intent);
     }
 
